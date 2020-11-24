9a94934a15dc9a98b84cf0d2a653e0f17fef9a04
Andrew M <a.melnik.work@gmail.com>
2020-11-24 15:55:19 +0200

    init

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="task_")
+ */
+class TaskController  extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(Request $request): Response
+    {
+        dd('index');
+    }
+}
